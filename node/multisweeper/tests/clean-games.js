8b45ac4a52d78b3e8d66b98a9fc57d3102c94989
Ingy döt Net <ingy@ingy.net>
2012-04-15 16:41:55 -0700

    extracted node/multisweeper into Stackato-Apps repo

--- node/multisweeper/tests/clean-games.js
+++
@@ -1,6 +0,0 @@
-var client = require("redis").createClient();
-
-client.del("games", function(err, data) {
-   console.log("err: " + err);
-   console.log("data: " + data);
-});
--- node/multisweeper/tests/clean-scores.js
+++
@@ -1,6 +0,0 @@
-var client = require("redis").createClient();
-
-client.del("scores", function(err, data) {
-   console.log("err: " + err);
-   console.log("data: " + data);
-});
