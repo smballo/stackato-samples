f962ee6345f0d35e0f341fb8675631af6e5a2966
Ingy döt Net <ingy@ingy.net>
2012-04-15 18:32:06 -0700

    extracted perl/bugzilla/ into Stackato-Apps repo

--- perl/bugzilla/myconfig.pl
+++
@@ -1,9 +0,0 @@
-# Sample Bugzilla configuration
-%answer = (
-    ADMIN_EMAIL    => 'admin@stackato.local',
-    ADMIN_PASSWORD => 'changeme',
-    ADMIN_REALNAME => 'Sample Admin',
-    mailfrom       => "bugzilla-daemon\@stackato.local",
-    smtpserver     => "stackato.local",
-    urlbase        => 'http://bugzilla.stackato.local',
-);
